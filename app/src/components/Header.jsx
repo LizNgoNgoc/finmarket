b6a9cf54229e007cb1ab8a06f154a794c9f40ab9
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-06-13 13:57:56 +0300

    add changes header.jsx

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -3,13 +3,18 @@ import styles from './header.module.css'
 export default function Header() {
     return <header className={styles.header}>
         <div className={styles.header_container}>
-            <img src="" alt="logo" />
+            <img src="./images/header/logo.svg" alt="logo" />
             <div className={styles.header_container_info}>
-                <p></p>
-                <p></p>
-                <img src="" alt="login" />
+                <p className={styles.header_phone}>8 (912) 364-52-53 </p>
+            <div className={styles.header_container_login}>
+                <p className={styles.header_text}>ВОЙТИ</p>
+                <img src="./images/header/LogIn.png" className={styles.header_login} alt="login" />
+            </div> 
             </div>
         </div>
-        <div></div>
+        <hr className={styles.header_line}/>
+        <nav className={styles.header_nav}>
+
+        </nav>
     </header>
 }
