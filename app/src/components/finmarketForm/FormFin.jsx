a2b3f1ce3520cf2f4e7b42f617e4a758a46e0393
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-06-17 08:34:16 +0300

    add changes

--- app/src/components/finmarketForm/FormFin.jsx
+++ app/src/components/finmarketForm/FormFin.jsx
@@ -1,11 +1,27 @@
+import styles from './formFin.module.css'
+import { Link } from 'react-router-dom'
+
 export default function FormFin() {
-    return <div>
-        <form action="">
-            <input type="text" />
-            <input type="phone" />
-            <input type="email" />
-            <input type="password" />
-            <input type="password" />
+    return <div className={styles.form_container}>
+        <nav className={styles.nav_form}>
+            <Link className={styles.link_nav}>ЗАЙМ</Link>
+            <Link className={styles.link_nav}>РЕФИНАНСИРОВАНИЕ</Link>
+            <Link className={styles.link_nav}>БАНКРОТСТВО</Link>
+            <Link className={styles.link_nav}>ВОЗВРАТ ДОЛГОВ</Link>
+        </nav>
+        <hr className={styles.form_line}/>
+        <form action="" className={styles.form}>
+            <input type="text" className={styles.form_inp}/>
+            <input type="phone" className={styles.form_inp}/>
+            <input type="email" className={styles.form_inp}/>
+            <input type="password" className={styles.form_inp}/>
+            <input type="password" className={styles.form_inp}/>
+        <div className={styles.form_confident}>
+            <input type="checkbox" />
+            <p className={styles.text_confident}>Я согласен на обработку данных согласно Пользовательскому соглашению и Политике конфиденциальности</p>
+        </div>
+        <button>участвовать</button>
         </form>
+        
     </div>
 }
--- app/src/pages/Finmarket.jsx
+++ app/src/pages/Finmarket.jsx
@@ -3,6 +3,12 @@ import styles from './finmarket.module.css'
 
 export default function Finmarket() {
     return <section className={styles.section_fin}> 
+        <div className={styles.fin_container}>
+            <img src="./images/finmatketPage/scobe_up.png" className={styles.scobe_up} alt="scobe" />
+            <img src="./images/finmatketPage/finmarketPro.png" className={styles.fin_pro} alt="finmarket" />
+            <img src="./images/finmatketPage/scobe_down.png" className={styles.scobe_down} alt="scobe" />
+            <p className={styles.text_fin}>Отправьте заявку на торги или создайте “Готовое предложение”.За вашу заявку будуь торговатьсяболее 100 организаций</p>
+        </div>
         <FormFin/>
     </section>
 }
