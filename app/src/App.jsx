34422e9e2b0237265bf113697b7e13cb12438354
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-06-17 09:19:35 +0300

    add new components

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Finmarket from './pages/Finmarket';
+import FinServiceRules from './components/finmarketServiceRules/FinServiceRules';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Finmarket/>
+      <FinServiceRules/>
       <Routes>
           
       </Routes>
--- app/src/components/finmarketForm/FormFin.jsx
+++ app/src/components/finmarketForm/FormFin.jsx
@@ -11,16 +11,16 @@ export default function FormFin() {
         </nav>
         <hr className={styles.form_line}/>
         <form action="" className={styles.form}>
-            <input type="text" className={styles.form_inp}/>
-            <input type="phone" className={styles.form_inp}/>
-            <input type="email" className={styles.form_inp}/>
-            <input type="password" className={styles.form_inp}/>
-            <input type="password" className={styles.form_inp}/>
+            <input type="text" placeholder='Получить займ' className={styles.form_inp}/>
+            <input type="phone" placeholder='Телефон' className={styles.form_inp}/>
+            <input type="email" placeholder='Email' className={styles.form_inp}/>
+            <input type="password" placeholder='Пароль' className={styles.form_inp}/>
+            <input type="password" placeholder='Повторите пароль' className={styles.form_inp}/>
         <div className={styles.form_confident}>
             <input type="checkbox" />
             <p className={styles.text_confident}>Я согласен на обработку данных согласно Пользовательскому соглашению и Политике конфиденциальности</p>
         </div>
-        <button>участвовать</button>
+        <button className={styles.form_btn}>УЧАСТВОВАТЬ</button>
         </form>
         
     </div>
