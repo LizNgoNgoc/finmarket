d479eaa3cd74bd2efecccf3a974685c333aa9d83
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-06-17 12:35:52 +0300

    add css steps

--- app/src/components/finmarketServiceRules/FinServiceRules.jsx
+++ app/src/components/finmarketServiceRules/FinServiceRules.jsx
@@ -2,7 +2,7 @@ import styles from './finServiceRules.module.css'
 
 export default function FinServiceRules() {
     return <section className={styles.service_container}>
-        <hr className={styles.service_line}/>
+        {/* <hr className={styles.service_line}/> */}
         <div className={styles.service_info}>
             <h3 className={styles.service_header}>Как получить услугу на FinMarket.pro</h3>
             <p className={styles.service_text}>FinMarket.pro — это первый сайт, на котором вы сможете получить как физ. или юр. лицо финансовую услугу на самых выгодных условиях только от проверенных организаций на Торгах по «Голландской системе» или создав готовое предложение на которое могут откликнуться для предоставления займа.</p>
@@ -11,19 +11,50 @@ export default function FinServiceRules() {
         <div className={styles.rules_container}>
             <div className={styles.rules_part}>
                 <div >
-                    <div>1</div>
-                    <div>
-                        <h3>Регистрация</h3>
-                        <p>Выберите роль: - получатель услуги - поставщик услугил</p>
+                    <div className={styles.step_container}>
+                        <div className={styles.num_container}>1</div>
+                        <p className={styles.num_step}>шаг</p>
+                    </div>
+                    <div className={styles.text_container}>
+                        <h3 className={styles.header_text}>Регистрация</h3>
+                        <p className={styles.paragraph_text}>Выберите роль: - получатель услуги - поставщик услугил</p>
                     </div>
                 </div>
-                <div></div>
+                <div>
+                    <div className={styles.step_container}>
+                        <div className={styles.num_container}>2</div>
+                        <p className={styles.num_step}>шаг</p>
+                    </div>
+                    <div className={styles.text_container}>
+                        <h3 className={styles.header_text}>Заполните анкету</h3>
+                        <p className={styles.paragraph_text}>Больше звездочек при заполнении - лучше результат</p>
+                    </div>
+                </div>
             </div>
             <div className={styles.rules_part}>
-                <div></div>
-                <div></div>
+                <div >
+                    <div className={styles.step_container}>
+                        <div className={styles.num_container}>3</div>
+                        <p className={styles.num_step}>шаг</p>
+                    </div>
+                    <div className={styles.text_container}>
+                        <h3 className={styles.header_text}>Отправьте лот на торги и участвуйто в аукционе</h3>
+                        <p className={styles.paragraph_text}>или</p>
+                        <h3 className={styles.header_text}>Создайте готовое предложение и получайте отклики</h3>
+                    </div>
+                </div>
+                <div>
+                    <div className={styles.step_container}>
+                        <div className={styles.num_container}>4</div>
+                        <p className={styles.num_step}>шаг</p>
+                    </div>
+                    <div className={styles.text_container}>
+                        <h3 className={styles.header_text}>Получайте результат и организуйте встречу</h3>
+                        <p className={styles.paragraph_text}>Конечный результат на FinMarket.pro это обмен контактными данными. Ждите звонка от победителя торгов и организуйте встречу</p>
+                    </div>
+                </div>
             </div>
         </div>
-        <hr className={styles.service_line} />
+        {/* <hr className={styles.service_line} /> */}
     </section>
 }
