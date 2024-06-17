8f92b9a3a8543a6e2d649d197a0bec8316473c0f
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-06-17 11:22:20 +0300

    add changes

--- app/src/components/finmarketServiceRules/FinServiceRules.jsx
+++ app/src/components/finmarketServiceRules/FinServiceRules.jsx
@@ -3,17 +3,24 @@ import styles from './finServiceRules.module.css'
 export default function FinServiceRules() {
     return <section className={styles.service_container}>
         <hr className={styles.service_line}/>
+        <div></div>
         <div className={styles.service_info}>
             <h3 className={styles.service_header}>Как получить услугу на FinMarket.pro</h3>
             <p className={styles.service_text}>FinMarket.pro — это первый сайт, на котором вы сможете получить как физ. или юр. лицо финансовую услугу на самых выгодных условиях только от проверенных организаций на Торгах по «Голландской системе» или создав готовое предложение на которое могут откликнуться для предоставления займа.</p>
             <b className={styles.service_btn}>ПОДРОБНЕЕ</b>
         </div>
-        <div>
-            <div>
-                <div></div>
+        <div className={styles.rules_container}>
+            <div className={styles.rules_part}>
+                <div>
+                    <div>1</div>
+                    <div>
+                        <h3>Регистрация</h3>
+                        <p>Выберите роль: - получатель услуги - поставщик услугил</p>
+                    </div>
+                </div>
                 <div></div>
             </div>
-            <div>
+            <div className={styles.rules_part}>
                 <div></div>
                 <div></div>
             </div>
