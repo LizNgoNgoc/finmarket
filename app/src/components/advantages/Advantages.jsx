f30a29d7732f8f9dbe60177f2f607dce9d065e6a
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-06-24 09:41:33 +0300

    add changes

--- app/src/components/advantages/Advantages.jsx
+++ app/src/components/advantages/Advantages.jsx
@@ -6,16 +6,34 @@ export default function Advantages() {
         <div className={styles.advantages_content}>
             <div className={styles.advantages_container}>
                 <div className={styles.adv_container}>
-                    <div></div>
-                    <div></div>
+                    <div className={styles.advantages_category}>
+                        <img src="" alt="" />
+                        <p>На бирже вы сможете получить лучшие условия  по финансовым услугам </p>
+                    </div>
+                    <div className={styles.advantages_category}>
+                        <img src="" alt="" />
+                        <p>За вашу анкету будут конкурировать более 100 поставщиков услуг</p>
+                    </div>
                 </div>
                 <div className={styles.adv_container}>
-                    <div></div>
-                    <div></div>
+                    <div className={styles.advantages_category}>
+                        <img src="" alt="" />
+                        <p>Индивидуальные условия - торги вы сами настариваете, по вашему усмотрению</p>
+                    </div>
+                    <div className={styles.advantages_category}>
+                        <img src="" alt="" />
+                        <p>Создайте предложение и на ваш лот откликнуться на торгах или как на готовое предложение</p>
+                    </div>
                 </div>
                 <div className={styles.adv_container}>
-                    <div></div>
-                    <div></div>
+                    <div className={styles.advantages_category}>
+                        <img src="" alt="" />
+                        <p>Юридическая поддержка системы</p>
+                    </div>
+                    <div className={styles.advantages_category}>
+                        <img src="" alt="" />
+                        <p>Конкуренция дает вам лучшее предложение</p>
+                    </div>
                 </div>
             </div>
             <div>
