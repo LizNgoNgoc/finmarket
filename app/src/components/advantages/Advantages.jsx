891364659cdc6b9bd461d210919d4c36ce7ce5c6
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-06-24 09:25:48 +0300

    add Advantages.jsx component

---
+++ app/src/components/advantages/Advantages.jsx
@@ -0,0 +1,15 @@
+import styles from './advantages.module.css'
+
+export default function Advantages() {
+    return <section>
+        <h2>Наши преимущества</h2>
+        <div>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div></div>
+        </div>
+    </section>
+}
--- app/src/components/ourServices/Services.jsx
+++ app/src/components/ourServices/Services.jsx
@@ -6,7 +6,22 @@ export default function Services() {
         <div className={styles.services_container}>
             <div className={styles.service_content}>
                 <h3 className={styles.header_srvcont}>ПОЛУЧЕНИЕ ЗАЙМА</h3>
-                <img src="./images/services/own.png" className={styles.img_servcont} alt="img" />
+                <img src="./images/services/own.png" className={styles.img_own} alt="img" />
+                <button className={styles.btn_servcont}>ПОДРОБНЕЕ</button>
+            </div>
+            <div className={styles.service_content}>
+                <h3 className={styles.header_srvcont}>РЕФИНАНСИРОВАНИЕ ЗАЙМА</h3>
+                <img src="./images/services/ref_own.png" className={styles.img_ref} alt="img" />
+                <button className={styles.btn_servcont}>ПОДРОБНЕЕ</button>
+            </div>
+            <div className={styles.service_content}>
+                <h3 className={styles.header_srvcont}>БАНКРОТСТВО</h3>
+                <img src="./images/services/crash.png" className={styles.img_crash} alt="img" />
+                <button className={styles.btn_servcont}>ПОДРОБНЕЕ</button>
+            </div>
+            <div className={styles.service_content}>
+                <h3 className={styles.header_srvcont}>ВОЗВРАТ ДОЛГОВ</h3>
+                <img src="./images/services/debt.png" className={styles.img_debt} alt="img" />
                 <button className={styles.btn_servcont}>ПОДРОБНЕЕ</button>
             </div>
         </div>
