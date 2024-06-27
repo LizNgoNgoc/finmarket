74765f70241c4187ee9afcd5c26c4f3d759cc6b1
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-06-27 12:22:15 +0300

    add changes

--- app/src/components/territory/Territory.jsx
+++ app/src/components/territory/Territory.jsx
@@ -1,22 +1,65 @@
+import styles from './territory.module.css'
+
 export default function Territory() {
-    return <section>
-        <h2>Мы представлены на всей территории РФ</h2>
-        <div>
-            <img src="" alt="img" />
-            <div>
-                <h3></h3>
-                <p></p>
+    return <section className={styles.territory}>
+        <h2 className={styles.territory_header}>Мы представлены на всей территории РФ</h2>
+        <div className={styles.img_container}>
+            <img src="./images/territory/card.png" className={styles.img} alt="img" />
+            <div className={styles.card_container}>
+                <h3 className={styles.header_card}>более 1000</h3>
+                <p className={styles.text_card}>готовы дать вам лучшее предложение</p>
             </div>
-            <div>
-                <h3></h3>
-                <p></p>
+            <div className={styles.card_container_cityes}>
+                <h3 className={styles.header_card}>55</h3>
+                <p className={styles.text_card}>городов присутствия</p>
             </div>
-            <div>
-                <h3></h3>
-                <p></p>
+            <div className={styles.card_container_clients}>
+                <h3 className={styles.header_card}>более 1000</h3>
+                <p className={styles.text_card}>клиентов получивших услугу</p>
             </div>
         </div>
-        
-        <div></div>
+        <hr className={styles.territory_line}/>
+        <div className={styles.coin_container}>
+            <div>
+                <h3 className={styles.name_coin}>USD</h3>
+                <p className={styles.value_coin}>60,8685</p>
+            </div>
+            <div>
+                <h3 className={styles.name_coin}>EUR</h3>
+                <p className={styles.value_coin}>60,8685</p>
+            </div>
+            <div>
+                <h3 className={styles.name_coin}>JPY</h3>
+                <p className={styles.value_coin}>60,8685</p>
+            </div>
+            <div>
+                <h3 className={styles.name_coin}>SEK</h3>
+                <p className={styles.value_coin}>60,8685</p>
+            </div>
+            <div>
+                <h3 className={styles.name_coin}>AUD</h3>
+                <p className={styles.value_coin}>60,8685</p>
+            </div>
+            <div>
+                <h3 className={styles.name_coin}>CNY</h3>
+                <p className={styles.value_coin}>60,8685</p>
+            </div>
+            <div>
+                <h3 className={styles.name_coin}>DKK</h3>
+                <p className={styles.value_coin}>60,8685</p>
+            </div>
+            <div>
+                <h3 className={styles.name_coin}>KRW</h3>
+                <p className={styles.value_coin}>60,8685</p>
+            </div>
+            <div>
+                <h3 className={styles.name_coin}>GBP</h3>
+                <p className={styles.value_coin}>60,8685</p>
+            </div>
+            <div>
+                <h3 className={styles.name_coin}>CHF</h3>
+                <p className={styles.value_coin}>60,8685</p>
+            </div>
+        </div>
     </section>
 }
