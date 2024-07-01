dea3f6c474688e5bafabf31f1f0e45184106b5ff
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-07-01 21:05:10 +0300

    add changes

--- app/src/components/onlineTrade/Completed.jsx
+++ app/src/components/onlineTrade/Completed.jsx
@@ -1,24 +1,16 @@
 import styles from './trade.module.css'
 
-export default function Completed() {
-    return <section className={styles.completed}>
-        <div className={styles.container_info}>
-            <div className={styles.less_container}>
-                <h3 className={styles.header_first}>Частный кредитор</h3>
-                <p className={styles.paragraph_container}>ставок нет </p>
+export default function Completed({item}) {
+    return <section className={`${styles.completed}`}>
+        <div  className={styles.container_info}>
+            {item.cardTrade.map((item, index) => {
+                return   <div key={index} className={index % 2 === 1 ? styles.bigger_container : styles.less_container}>
+                    <h3 className={styles.header_first}>{item.title}</h3>
+                    <p className={styles.paragraph_container}>{item.paragraph}</p>
+                 </div>
+                })
+            }
             </div>
-            <div className={styles.bigger_container}>
-                <h3 className={styles.header_second}>Кредитный брокер</h3>
-                <p className={styles.paragraph_container}>последняя ставка</p>
-                <p className={styles.percent}>25.0%</p>
-                <p className={styles.win}>Победитель: ООО "КРЕДИТМАРКЕТ+"</p>
-            </div>
-            <div className={styles.less_sec_container}>
-                <h3 className={styles.header_third}>Ломбард</h3>
-                <p className={styles.paragraph_container}>ставка</p>
-                <p className={styles.percent}>25.5%</p>
-            </div>
-        </div>
         <button className={styles.watch_btn}>ПОСМОТРЕТЬ</button>
     </section>
 }
--- app/src/components/onlineTrade/Trade.jsx
+++ app/src/components/onlineTrade/Trade.jsx
@@ -1,7 +1,7 @@
 import styles from './trade.module.css'
 import { Link } from 'react-router-dom'
 import { trade } from './TradeObjects'
-import Completed from './Completed'
+import TradeBlock from './TradeBlock'
 
 export default function Trade() {
    
@@ -31,46 +31,10 @@ export default function Trade() {
                     <div className={styles.empty}></div>
                 </div>
             {trade.map((item, index) => {
-                return <div key={index} className={styles.table_flex}>
-                    <hr className={styles.line_category}/>
-                        <div className={styles.table_names}>
-                <p className={styles.table_td}>211</p>
-            <div className={styles.table_td}>
-                <p className={styles.table_td}>Займ</p>
-                <div className={styles.provision}>
-                    <img src={item.imgCat} alt="category" />
-                    <p className={styles.table_td}>{item.nameCat}</p>
-                </div>
-            </div>
-                <div className={styles.table_td}>
-                    <p className={styles.table_td}>{item.price}</p>
-                <div className={styles.container_category}>
-                    <img src="./images/offers/Time.png"  className={styles.img_time} alt="time" />
-                    <p className={styles.table_td}>{item.month} мес</p>
-                </div>
-                </div>
-                <div className={styles.table_td}>
-                    <p className={styles.table_td}>{item.borrow}</p>
-                    <div>
-                        {Array(5).fill().map((e, index) => {
-                            return <img key={index} src={index > item.rating - 1  ? './images/offers/StarGrey.png' : './images/offers/StarYellow.png'}  alt="stars" />
-                        })}
-                    </div>
-                </div>
-                <div className={styles.table_td}>
-                    <p className={styles.table_td}>ломбард</p>
-                    <p className={styles.table_td}>кредитный брокер</p>
-                </div>
-                <p className={styles.table_td}>8% годовых</p>
-                <p className={styles.table_td}>{item.city}</p>
-                <p className={styles.table_td}>{item.status}</p>
-                <button className={styles.table_btn}>РАЗВЕРНУТЬ</button>
-            </div>
-            </div>
+                return <TradeBlock  key={index} item={item}/>
             })}
             <hr className={styles.line_category} />
             <button className={styles.archive_btn}>АРХИВ ТОРГОВ</button>
             </div>
-        <Completed/>
     </section>
 }
