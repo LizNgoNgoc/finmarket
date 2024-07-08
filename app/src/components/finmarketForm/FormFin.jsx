4e21bb9941ac411e2223f4c64760c4907cd77d6e
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-07-08 21:05:27 +0300

    add changes

--- app/src/components/finmarketForm/FormFin.jsx
+++ app/src/components/finmarketForm/FormFin.jsx
@@ -3,37 +3,26 @@ import styles from './formFin.module.css'
 import { Link } from 'react-router-dom'
 
 export default function FormFin() {
-    const[formElements, setFormElements] = useState({select : "Получить займ" })
-    const [formErrors, setFormErrors] = useState({
-        phone: 'error',
-        email: 'error',
-        password: 'error'
-    })
-
+    const[formElements, setFormElements] = useState({})
+    const [formErrors, setFormErrors] = useState({})
     useEffect(() => {
-        formElements.phone = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
-        formElements.email = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-        formElements.password = /(?=.*[0-9])(?=.*[a-z])[0-9a-zA-Z]{6,}/g;
-
-        formErrors.phone = 'error';
-        formErrors.email = 'error';
-        formErrors.password = 'error';
+       const arrKeys = Object.keys(formElements)
+        arrKeys.forEach(key => {
+            setFormErrors((prev) => ({...prev, [key] : formElements[key].value.length > 0 ? '' : 'Поле не должно быть пустым'}))  
+        })
     }, [formElements])
 
     function handleChange(e) {
-        setFormElements({...formElements, [e.target.name]: e.target.value})
+        setFormElements({...formElements, [e.target.name]: e.target})
     }
-
-    // function handleError(e) {
-    //     const valid = !formErrors(e)
-    //     setFormErrors({...formErrors, [e.target.name] : valid})
-    // }
-
-    //onInput={(e) => handleError(e, formErrors.phone)}
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(formElements);
+        const body = {}
+        for(let key in formElements){
+            body[key] = formElements[key].value
+        }
+        console.log(body);
     }
 
     return <div className={styles.form_container}>
@@ -45,20 +34,21 @@ export default function FormFin() {
         </nav>
         <hr className={styles.form_line}/>
         <form onSubmit={handleSubmit} className={styles.form}>
-            <select  onChange={(e) => setFormElements({...formElements, [e.target.name]: e.target.value})} name='select' className={styles.form_inp}>
+            <select  onChange={handleChange} name='select' className={styles.form_inp}>
+                <option value="" className={styles.option}></option>
                 <option value="Получить займ" className={styles.option}>Получить займ</option>
                 <option value="Рефинансирование займа" className={styles.option}>Рефинансирование займа</option>
                 <option value="Банкротство" className={styles.option}>Банкротство</option>
                 <option value="Возврат долгов" className={styles.option}>Возврат долгов</option>
             </select>
-            <input type="phone" name="phone" placeholder='Телефон' onInput={handleChange} className={styles.form_inp}/>
-            <p className={`${styles.error} ${formElements.phone ? styles.show : ''}`}>{formErrors.phone}</p>
-            <input type="email" name="email" placeholder='Email' onInput={(e) => setFormElements({...formElements, [e.target.name]: e.target.value})} className={styles.form_inp}/>
+            <input type="phone" name="phone" placeholder='Телефон' onInput={handleChange} className={styles.form_inp} required/>
+            <p className={`${styles.error}`}>{formErrors.phone}</p>
+            <input type="email" name="email" placeholder='Email' onInput={handleChange} className={styles.form_inp} required/>
             <p className={styles.error}>{formErrors.email}</p>
-            <input type="password" name="password"  onInput={(e) => setFormElements({...formElements, [e.target.name]: e.target.value})} placeholder='Пароль' className={styles.form_inp}/>
+            <input type="password" name="password"  onInput={handleChange} placeholder='Пароль' className={styles.form_inp} required/>
             <p className={styles.error}>{formErrors.password}</p>
         <div className={styles.form_confident}>
-            <input type="checkbox" name='remember'  onChange={(e) => setFormElements({...formElements, [e.target.name]: e.target.checked})} className={styles.form_checkbox}/>
+            <input type="checkbox" name='remember' onChange={handleChange} className={styles.form_checkbox}/>
             <p className={styles.text_confident}>Я согласен на обработку данных согласно Пользовательскому соглашению и Политике конфиденциальности</p>
         </div>
         <button className={styles.form_btn}>УЧАСТВОВАТЬ</button>
