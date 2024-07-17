52920e6647016b66049751beada90e92e1e316da
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-07-17 13:01:26 +0300

    add Svheme component.jsx

---
+++ app/src/components/serviceScheme/Scheme.jsx
@@ -0,0 +1,57 @@
+import styles from './scheme.module.css'
+
+export default function Scheme() {
+    return <section className={styles.scheme}>
+        <div className={styles.scheme_container}>
+            <div className={styles.scheme_content}>
+                <div className={styles.container_info}>
+                    <h3 className={styles.scheme_header}>Схема получения наших услуг  </h3>
+                    <p className={styles.scheme_text}>UFEx.pro — это первый сайт, на котором вы получите необходимую сумму 
+                        на самых выгодных условиях только от проверенных организаций и частных инвесторов 
+                        на Торгах по «Голландской системе» или создав готовое предложение на которое могут 
+                        откликнуться для предоставления займа.</p>
+                    <p className={styles.scheme_text}>«Голландская система торгов» (англ. Dutch auction) – система, при которой ставка по услуге 
+                        предоставляется на понижение процентной ставки по запрашиваемой услуге. Покупатель, который 
+                        предоставил наименьшую и последнюю процентную ставку в зависимости от временного типа торгов 
+                        объявляется Победителем.</p>
+                </div>
+            </div>
+            <div className={styles.container_boxes}>
+                <div className={styles.box}>
+                    <div className={styles.box_item}>
+                        <h3 className={styles.header_box}>1 шаг</h3>
+                        <h3 className={styles.header_box}>Регистрация на сайте</h3>
+                        <p className={styles.text_box}>Создайте личный кабинет, указав электронную почту и номер телефона</p>
+                    </div>
+                    <div className={styles.box_item}>
+                        <h3 className={styles.header_box}>2 шаг</h3>
+                        <h3 className={styles.header_box}>Заполние анкеты</h3>
+                        <p className={styles.text_box}>Внесите необходимые данные в анкету, отправьте ее на торги или 
+                            создайте готовое предложение и отправьте организациям: 
+                            частный инвестор, кредитный брокер, ломбард</p>
+                    </div>
+                </div>
+                <div className={styles.ordinary_box}>
+                        <h3 className={styles.header_box}>3 шаг</h3>
+                        <h3 className={styles.header_box}>Торги/Создание готовых предложений</h3>
+                        <p className={styles.text_box}>За вашу анкету (Лот) пройдут торги на понижение процентной ставки и 
+                            предоставление лучших условий для получения займа. При Готовых предложений 
+                            вы создаете готовое предложение, на которое откликается соответствующая организация: 
+                            частный инвестор, кредитный брокер, ломбард.</p>
+                </div>
+                <div className={styles.box}>
+                    <div className={styles.box_item}>
+                        <h3 className={styles.header_box}>4 шаг</h3>
+                        <h3 className={styles.header_box}>Обмен информацией по результатам торгов</h3>
+                        <p className={styles.text_box}>Обменяйтесь данными с кредитной организацией, частным инвестором или ломбардом и заключите договор займа.</p>
+                    </div>
+                    <div className={styles.box_item}>
+                        <h3 className={styles.header_box}>5 шаг</h3>
+                        <h3 className={styles.header_box}>Получение займа</h3>
+                        <p className={styles.text_box}>Получите деньги на самых выгодных для вас условиях.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+}
--- app/src/pages/Borrow.jsx
+++ app/src/pages/Borrow.jsx
@@ -1,39 +1,12 @@
 import styles from './borrow.module.css'
+import BorrowInfo from '../components/borrowInfo/BorrowInfo'
+import Scheme from '../components/serviceScheme/Scheme'
 
 export default function Borrow() {
     return <section className={styles.borrow}>
-        <div className={styles.links}>
-            <span className={styles.span_header}>Главная / Услуги / </span><span className={styles.span_page}>Получение займа</span>
-        </div>
-        <div className={styles.info_container}>
-            <div className={styles.borrow_info}>
-                <h3 className={styles.borrow_header}>Получение займа</h3>
-            <div className={styles.text_container}>
-                <p className={styles.borrow_text}>Займ — это форма договора, по которому одна сторона (заимодавец) предоставляет 
-                    в долг денежные средства другой стороне (заемщику). Согласно договору займа денежные средства могут выдаваться 
-                    как под процентную ставку, так и под залог движимого или недвижимого имущества.</p>
-                <p className={styles.borrow_text}>В договоре займа прописываются все условия и сроки, 
-                    при невыполнении которых заемщик имеет право взыскать задолженность через суд или же конфисковать залог.</p>
-             </div> 
-                <button className={styles.borrow_btn}>получить услугу</button>
-            </div>
-            <div className={styles.img_container}>
-                <img src="./images/borrow/borrow_main.png" alt="img" />
-            </div>
-        </div>
-        <div className={styles.rules_info}>
-            <div className={styles.rules_cont}>
-                <h3 className={styles.header_rules}>Общие требования к заемщику:</h3>
-                <p className={styles.text_rules}>Наличие паспорта.</p>
-                <p className={styles.text_rules}>Возраст не менее 18 лет.</p>
-                <p className={styles.text_rules}>В некоторых случаях кредитор имеет право расширить список документов, предоставляемых заемщиком.</p>
-            </div>
-            <div className={styles.rules_cont}>
-                <h3 className={styles.header_rules}>Порядок получения займа:</h3>
-                <p className={styles.text_rules}>Предоставление кредитору от заёмщика данных или иной необходимой информации.</p>
-                <p className={styles.text_rules}>Заключение договора между сторонами.</p>
-                <p className={styles.text_rules}>Получение денег.</p>
-            </div>
-        </div>
+        <BorrowInfo/>
+        <hr className={styles.line}/>
+        <Scheme/>
+        <hr className={styles.line}/>
     </section>
 }
