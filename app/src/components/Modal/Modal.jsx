27519974c2eaa74a23c961ac5744d1868a830ade
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-08-03 12:16:38 +0300

    add modal service styles

--- app/src/components/Modal/Modal.jsx
+++ app/src/components/Modal/Modal.jsx
@@ -9,6 +9,20 @@ export default function Modal ({open}) {
             <div className={styles.nav_content}>регистрация</div>
         </nav>
         <hr className={styles.line}/>
+        <div className={styles.container}>
+            <div className={styles.step}>
+                <div className={styles.number}>1</div>
+                <div className={styles.text_container}>
+                    <p className={styles.steptxt}>шаг</p>
+                </div>
+            </div>
+            <div className={styles.srvice_container}>
+                <div className={styles.get}>Получить услугу</div>
+                <div className={styles.take}>Предоставить услугу</div>
+            </div>
+            <div></div>
+            <form action=""></form>
+        </div>
         </div>
     </section>
 }
